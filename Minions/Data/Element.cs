ddc09405dc0a46bf08491617e430d046002991aa
Kay Schönheits <turok121@web.de>
2023-11-01 12:27:13 +0100

    GOGO

--- Minions/Data/Element.cs
+++ Minions/Data/Element.cs
@@ -2,12 +2,12 @@
 
 public class Element
 {
-    public required string Name { get; set; }
+    public string? Name { get; set; }
     public int Age { get; set; }
-    public required string Race { get; set; }
-    public required string Sex { get; set; }
+    public string? Race { get; set; }
+    public string? Sex { get; set; }
     public float Payment { get; set; }
-    public required string Job { get; set; }
+    public string? Job { get; set; }
     public float CurrentPayment => Payment * DayWihtoutPay;
     public int DayWihtoutPay { get; set; }
 
