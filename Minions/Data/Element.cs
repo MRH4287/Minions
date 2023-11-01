9d6a16904d362ab2fe4cccf70721444f5624d7db
Kay Schönheits <turok121@web.de>
2023-11-01 09:02:32 +0100

    Erster Commit

---
+++ Minions/Data/Element.cs
@@ -0,0 +1,14 @@
+﻿namespace Minions.Data;
+
+public class Element
+{
+    public required string Name { get; set; }
+    public int Age { get; set; }
+    public required string Race { get; set; }
+    public required string Sex { get; set; }
+    public float Payment { get; set; }
+    public required string Job { get; set; }
+    public float CurrentPayment => Payment * DayWihtoutPay;
+    public int DayWihtoutPay { get; set; }
+
+}
